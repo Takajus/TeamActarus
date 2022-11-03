e908e2ec80609e7171649c0d37b9ec362b497861
loickGombauld <wild1907@hotmail.fr>
2022-11-03 09:46:35 +0100

    actarus conflit

--- Assets/Teams/Actarus/ExampleController.cs
+++ Assets/Teams/Actarus/ExampleController.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using DoNotModify;
 
-namespace ExampleTeam {
+namespace Actarus {
 
-	public class ExampleController : BaseSpaceShipController
-	{
+    public class ExampleController : BaseSpaceShipController
+    {
 
-		public override void Initialize(SpaceShipView spaceship, GameData data)
-		{
-		}
+        public override void Initialize(SpaceShipView spaceship, GameData data)
+        {
+        }
 
-		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
-		{
-			SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
-			float thrust = 1.0f;
-			float targetOrient = spaceship.Orientation + 90.0f;
-			bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
-			return new InputData(thrust, targetOrient, needShoot, false, false);
-		}
-	}
+        public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
+        {
+            SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+            float thrust = 1.0f;
+            float targetOrient = spaceship.Orientation + 90.0f;
+            bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
+            return new InputData(thrust, targetOrient, needShoot, false, false);
+        }
+    }
 
 }
