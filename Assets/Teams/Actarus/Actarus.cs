45c5b051afc1a3f0740c97372f6437e07c22f169
karimhomsi <karimal-homsi@hotmail.com>
2022-11-03 09:23:26 +0100

    change name space

--- Assets/Teams/Actarus/Actarus.cs
+++ Assets/Teams/Actarus/Actarus.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using DoNotModify;
 
-namespace ExampleTeam {
+namespace Actarus {
 
 	public class ExampleController : BaseSpaceShipController
 	{
